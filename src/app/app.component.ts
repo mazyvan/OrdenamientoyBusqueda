dd4e2f3b45a27cde8804a94763985d490cfb0a5c
Iván Eduardo Martínez Sánchez <mazyvan@hotmail.com>
2017-01-19 12:47:41 -0600

    guardando cambios burbuja done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  public data;
+
+  constructor() {
+  }
+
+  convertToArray(data: string) {
+    let list = data.split(",");
+    return list.filter(data => /\S/.test(data))
+      .map(data => Number(data))
+      .filter(data => (typeof data == "number" && data));
+  }
+
+  burbuja() {
+    let list = this.convertToArray(this.data);
+    function swap() {
+      list.forEach((number, i) => {
+        if (number > list[i + 1]) {
+          list[i] = list[i + 1];
+          list[i + 1] = number;
+        }
+      });
+    }
+    list.forEach(() => swap());
+    console.log("los datos son ordenados son: ", list);
+  }
+
+  insercion() {
+    let list = this.convertToArray(this.data);
+    let aux: number;
+    
+
+  }
+
+  seleccion() {
+    let list = this.convertToArray(this.data);
+
+  }
+
+  mezcla() {
+    let list = this.convertToArray(this.data);
+  }
+
+  casilleros() {
+    let list = this.convertToArray(this.data);
+  }
+
+  rapido() {
+    let list = this.convertToArray(this.data);
+  }
+
+  radix() {
+    let list = this.convertToArray(this.data);
+  }
+
 }
