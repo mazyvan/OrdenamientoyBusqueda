580871be7b20c2792d7e96ccd446d595df528471
Iván Eduardo Martínez Sánchez <mazyvan@hotmail.com>
2017-01-21 10:19:33 -0600

    funcionando insercion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,36 +7,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public data;
-
+  public list: number[] = [];
+  public unOrderList: number[] = [];
+  public iterations:any = [];
+  public iteration1 = "";
   constructor() {
   }
 
   convertToArray(data: string) {
+    data = data.replace("[","").replace("]","");;
     let list = data.split(",");
-    return list.filter(data => /\S/.test(data))
+    let lista = list.filter(data => /\S/.test(data))
       .map(data => Number(data))
       .filter(data => (typeof data == "number" && data));
+    this.unOrderList = lista.filter(() => true);
+    return lista;
   }
 
   burbuja() {
+    let self = this;
+    this.iterations = [];
     let list = this.convertToArray(this.data);
-    function swap() {
+    list.forEach(() => {
       list.forEach((number, i) => {
         if (number > list[i + 1]) {
           list[i] = list[i + 1];
           list[i + 1] = number;
         }
+        self.iterations.push(list.filter(() => true));
       });
-    }
-    list.forEach(() => swap());
-    console.log("los datos son ordenados son: ", list);
+    });
+    this.list = list;
+    console.log("los datos ordenados son: ", list);
   }
 
   insercion() {
+    let self = this;
+    this.iterations = [];
     let list = this.convertToArray(this.data);
     let aux: number;
-    
-
+    list.forEach((iteration, p) => {
+      aux = iteration;
+      for (var h = p - 1; h >= 0 && list[h] > aux; h--) {
+        var number = list[h];
+        list[h + 1] = number
+        list[h] = aux;
+        self.iterations.push(list.filter(() => true));
+      }
+    });
+    this.list = list;
+    console.log("los datos ordenados son: ", list);
   }
 
   seleccion() {
